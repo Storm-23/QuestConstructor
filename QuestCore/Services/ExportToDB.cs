d4599b9fa7358c93065329cedb1e0bca07bfa807
Storm <google@ukr.net>
2018-11-20 09:38:18 +0200

    Update ExportToDB.cs
    Additional fileds in DB export implemented.

--- QuestCore/Services/ExportToDB.cs
+++ QuestCore/Services/ExportToDB.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.OleDb;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 
 namespace QuestCoreNS
@@ -12,15 +14,33 @@ namespace QuestCoreNS
         public string TableName { get; set; } = "Лист1$";
         public bool ExportAltTextInsteadOfCode { get; set; } = false;
 
-        public void Export(Questionnaire questionnaire, Anketa anketa)
+        private const string ADD_PARAM_PREFIX = "@add";
+
+        public void Export(Questionnaire questionnaire, Anketa anketa, List<Tuple<string, object>> additionalParameters = null)
         {
             using (var conn = new OleDbConnection(ConnectionString))
             {
                 //формируем список колонок
                 var columnsList = string.Join(", ", questionnaire.Select(q => q.Id));
 
+                //имена колонок для дополнительных параметров
+                if (additionalParameters != null)
+                {
+                    columnsList += ", " + string.Join(", ", additionalParameters.Select(p => p.Item1));
+                    columnsList = columnsList.Trim(' ', ',');
+                }
+
                 //формируем список параметров
                 var paramsList = string.Join(", ", questionnaire.Select(q => "@" + q.Id));
+
+                //список доп параметров
+                if (additionalParameters != null)
+                {
+                    paramsList += ", " + string.Join(", ", Enumerable.Range(0, additionalParameters.Count).Select(p=> ADD_PARAM_PREFIX + p));
+                    paramsList = paramsList.Trim(' ', ',');
+                }
+
+                //формируем SQL запрос
                 var sql = string.Format("INSERT INTO [{2}]({0}) VALUES({1});", columnsList, paramsList, TableName);
 
                 //создаем команду
@@ -32,6 +52,13 @@ namespace QuestCoreNS
                         var answer = anketa.FirstOrDefault(a => a.QuestId == q.Id);
                         var val = GetExportedAlternativeValue(q, answer);
                         command.Parameters.AddWithValue("@" + q.Id, val);
+                    }
+
+                    //инициализируем доп параметры значениями
+                    if(additionalParameters != null)
+                    for (int i = 0; i < additionalParameters.Count; i++)
+                    {
+                        command.Parameters.AddWithValue(ADD_PARAM_PREFIX + i, additionalParameters[i].Item2);
                     }
 
                     //исполняем SQL запрос
